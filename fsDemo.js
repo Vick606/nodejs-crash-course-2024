1d63cb65f6097a199633e546f617d699956bc4a5
Victor Koech <vickoch20@gmail.com>
2024-07-27 20:28:24 +0300

    Add Promise .then() version of reading files

--- fsDemo.js
+++ fsDemo.js
@@ -1,11 +1,17 @@
-import fs from 'fs';
+// import fs from 'fs';
+import fs from 'fs/promises';
 
 // readFile() - callback
-fs.readFile('./test.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
+// fs.readFile('./test.txt', 'utf8', (err, data) => {
+    // if (err) throw err;
+    // console.log(data);
+// });
 
 // readFileSync() - Synchronous version
-const data = fs.readFileSync('./test.txt', 'utf8');
-console.log(data);
+// const data = fs.readFileSync('./test.txt', 'utf8');
+// console.log(data);
+
+// readFile() - Promise .then()
+fs.readFile('./test.txt', 'utf8')
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
