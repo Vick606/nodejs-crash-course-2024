646782eff958cc3fbda1e6859867c89fc4c1887e
Victor Koech <vickoch20@gmail.com>
2024-07-27 15:44:48 +0300

    Add routing Homepage and About

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
-
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end('<h1>Hello World, Node course in session<h1/>');
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>Homepage<h1/>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end('<h1>About<h1/>');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html'});
+        res.end('<h1>Not Found<h1/>');
+    }
 });
 
 server.listen(PORT, () => {
