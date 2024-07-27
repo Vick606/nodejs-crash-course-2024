5a0d6118bbbf72256f8d71a05abc06e2c147af2f
Victor Koech <vickoch20@gmail.com>
2024-07-27 16:54:42 +0300

    Add route not found

--- server2.js
+++ server2.js
@@ -14,6 +14,11 @@ const server = createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify(users));
         res.end();
+     } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: 'Route not found' }));
+        res.end();
      }
 });
 
