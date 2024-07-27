b461ac7dba6f8312ab0591207dba2e40e6a95862
Victor Koech <vickoch20@gmail.com>
2024-07-27 22:58:06 +0300

    Add params limit

--- urlDemo.js
+++ urlDemo.js
@@ -19,4 +19,7 @@ console.log(url.fileURLToPath(import.meta.url));
 console.log(urlObj.search);
 
 const params = new URLSearchParams(urlObj.search);
+console.log(params.get('q'));
+params.append('limit', '5');
 console.log(params);
+
