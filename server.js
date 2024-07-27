ef32cbf460a0ba8f684929494cb9c9c9e70772d1
Victor Koech <vickoch20@gmail.com>
2024-07-27 14:04:31 +0300

    Add writeHead

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 404;
-    res.end('<h1>Hello World, coding in session<h1>');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+
+    res.writeHead(500, { 'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ message: 'Server Error' }));
 });
 
 server.listen(PORT, () => {
