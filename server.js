a599c392b4e3704c2269e71539eed1904529a8fd
Victor Koech <vickoch20@gmail.com>
2024-07-27 15:58:57 +0300

    Update to check if GET request

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end('<h1>Homepage<h1/>');
-    } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end('<h1>About<h1/>');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html'});
-        res.end('<h1>Not Found<h1/>');
+    try {
+        // Check if GET request
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end('<h1>Homepage<h1/>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, { 'Content-Type': 'text/html'});
+                res.end('<h1>About<h1/>');
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html'});
+                res.end('<h1>Not Found<h1/>');
+            }
+
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain'});
+        res.end('Server Error');
     }
+
 });
 
 server.listen(PORT, () => {
