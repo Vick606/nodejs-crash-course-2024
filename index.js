87fc5b07ad1da175de037e936b0dd94af8ad431b
Victor Koech <vickoch20@gmail.com>
2024-07-27 13:21:01 +0300

    Add function to print posts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { getPosts } from "./postController.js";
 
+console.log(getPosts());
 // const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
 
 // console.log(`Random Number: ${generateRandomNumber ()}`);
