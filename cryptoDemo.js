1c6207f75bfb62e4bdef9cdd5e980e81b1a4f1b1
Victor Koech <vickoch20@gmail.com>
2024-07-27 23:09:11 +0300

    Add randomBytes()

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -4,3 +4,9 @@ import crypto from 'crypto';
 const hash = crypto.createHash('sha256');
 hash.update('password12345');
 console.log(hash.digest('hex'));
+
+// randomBytes()
+crypto.randomBytes(16, (err, buf) => {
+    if (err) throw err;
+    console.log(buf.toString('hex'));
+});
