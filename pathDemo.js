bb6a1f5e55d2994db188037d0d4b8d49bd2f7002
Victor Koech <vickoch20@gmail.com>
2024-07-27 22:27:27 +0300

    Add resolve()

--- pathDemo.js
+++ pathDemo.js
@@ -21,3 +21,7 @@ const __dirname = path.dirname(__filename);
 // join()
 const filePath2 = path.join(__dirname, 'dir1', 'dir2', 'test.txt');
 console.log(filePath2);
+
+// resolve 
+const filePath3 = path.resolve(__dirname, 'dir1', 'dir2', 'test.txt');
+console.log(filePath3);
