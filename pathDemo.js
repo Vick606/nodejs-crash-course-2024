a907d407815abb77379b24babeec3781579e790b
Victor Koech <vickoch20@gmail.com>
2024-07-27 21:58:32 +0300

    Add fileURLToPath and path.__dirname for ES modules

--- pathDemo.js
+++ pathDemo.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import url from 'url';
 
 const filePath = '.dir1/dir2/test.txt';
 
@@ -13,3 +14,7 @@ console.log(path.extname(filePath));
 
 // parse()
 console.log(path.parse(filePath));
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname);
