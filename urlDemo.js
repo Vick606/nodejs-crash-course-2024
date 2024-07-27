fa19aca14e1a15907501327b31b9e842134b1ea4
Victor Koech <vickoch20@gmail.com>
2024-07-27 22:50:19 +0300

    Add format(), import.meta.url(), and fileURLToPath()

--- urlDemo.js
+++ urlDemo.js
@@ -6,3 +6,9 @@ const urlString = 'https://google.com/search?q=hello+world';
 const urlObj = new URL(urlString);
 
 console.log(urlObj);
+
+// format()
+console.log(url.format(urlObj));
+
+// import.meta.url - file url
+console.log(import.meta.url);
