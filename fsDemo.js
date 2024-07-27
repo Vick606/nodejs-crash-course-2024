c87b49a813e50f7deb8e5e37ffffef36c6dc070f
Victor Koech <vickoch20@gmail.com>
2024-07-27 20:21:46 +0300

    Add syncronous version of reading files

--- fsDemo.js
+++ fsDemo.js
@@ -5,3 +5,7 @@ fs.readFile('./test.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+
+// readFileSync() - Synchronous version
+const data = fs.readFileSync('./test.txt', 'utf8');
+console.log(data);
