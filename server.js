f74a6447add6eb83c5274e9c0b0f96ae62e2856b
Victor Koech <vickoch20@gmail.com>
2024-07-27 16:25:57 +0300

    Add await fs.readFile(filePath)

--- server.js
+++ server.js
@@ -8,23 +8,24 @@ const PORT = process.env.PORT;
  const __filename = url.fileURLToPath(import.meta.url);
  const __dirname = path.dirname(__filename);
 
- console.log(__filename, __dirname);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
     try {
         // Check if GET request
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h1>Homepage<h1/>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, { 'Content-Type': 'text/html'});
-                res.end('<h1>About<h1/>');
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, { 'Content-Type': 'text/html'});
-                res.end('<h1>Not Found<h1/>');
+               throw new Error('Not Found') 
             }
-
+            
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             throw new Error('Method not allowed');
         }
