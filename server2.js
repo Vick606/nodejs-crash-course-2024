2ee62fb08ec239c3abd9b2ea29d18f5333298014
Victor Koech <vickoch20@gmail.com>
2024-07-27 18:01:51 +0300

    Remove repeated res.setHeader

--- server2.js
+++ server2.js
@@ -17,12 +17,11 @@ const server = createServer((req, res) => {
     } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[3];
         const user = users.find((user) => user.id === parseInt(id));
+        res.setHeader('Content-Type', 'application/json');
         if (user) {
-            res.setHeader('Content-Type', 'application/json');
             res.write(JSON.stringify(user));
             res.end();
         } else {
-            res.setHeader('Content-Type', 'application/json');
             res.statusCode = 404;
             res.write(JSON.stringify({ message: 'User not found' }));
             res.end();
