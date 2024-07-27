e7ecedb9d8b14935b4d6bfa96e1fcd0ad22bbd91
Victor Koech <vickoch20@gmail.com>
2024-07-27 23:25:05 +0300

    Update encrypted and decrypted logic

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -1,15 +1,15 @@
 import crypto from 'crypto';
 
 // createHash()
-const hash = crypto.createHash('sha256');
-hash.update('password12345');
-console.log(hash.digest('hex'));
+// const hash = crypto.createHash('sha256');
+// hash.update('password12345');
+// console.log(hash.digest('hex'));
 
 // randomBytes()
-crypto.randomBytes(16, (err, buf) => {
-    if (err) throw err;
-    console.log(buf.toString('hex'));
-});
+// crypto.randomBytes(16, (err, buf) => {
+    // if (err) throw err;
+    // console.log(buf.toString('hex'));
+// });
 
 // createCipheriv & createDecipheriv
 const algorithm = 'aes-s256-cbc';
@@ -20,3 +20,8 @@ const cipher = crypto.createCipheriv(algorithm, key, iv);
 let encrypted = cipher.update('Helo, this is a secrete message', 'utf8', 'hex');
 encrypted += cipher.final('hex');
 console.log(encrypted);
+
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, 'utf8', 'hex');
+decrypted += decipher.final('utf8');
+console.log(decrypted);
