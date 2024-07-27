c3660204fa11bddf39ff4a3e43bdb751e085226e
Victor Koech <vickoch20@gmail.com>
2024-07-27 23:19:16 +0300

    Update

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -10,3 +10,13 @@ crypto.randomBytes(16, (err, buf) => {
     if (err) throw err;
     console.log(buf.toString('hex'));
 });
+
+// createCipheriv & createDecipheriv
+const algorithm = 'aes-s256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update('Helo, this is a secrete message', 'utf8', 'hex');
+encrypted += cipher.final('hex');
+console.log(encrypted);
